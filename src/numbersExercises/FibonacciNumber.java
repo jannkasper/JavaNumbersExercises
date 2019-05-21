7b35d6600a7859102194ff99dd0f6801328770b2
Jan K <jann.kasper@gmail.com>
2019-05-21 05:40:13 +0200

    FibonacciNumber finish

--- src/numbersExercises/FibonacciNumber.java
+++ src/numbersExercises/FibonacciNumber.java
@@ -1,7 +1,24 @@
 package numbersExercises;
 
 public class FibonacciNumber {
-    public boolean checkFibonacci (int numer) {
+    public boolean checkFibonacci (int number) {
+
+        int i = 1;
+        int fibNumber = isFibonacci(i++);
+        while (fibNumber != number){
+            if (fibNumber > number) return false;
+            fibNumber = isFibonacci(i++);
+        }
         return true;
+    }
+
+
+
+    public int isFibonacci (int seriesPosition) {
+        if (seriesPosition == 1 || seriesPosition == 2) {
+            return 1;
+        } else {
+            return isFibonacci(seriesPosition - 1) + isFibonacci(seriesPosition - 2);
+        }
     }
 }
