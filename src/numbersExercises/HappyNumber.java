e7ff4d0d62d6be56f8f41a0513b5c487a5e7a079
Jan K <jann.kasper@gmail.com>
2019-05-21 06:48:40 +0200

    HappyNumber finished

--- src/numbersExercises/HappyNumber.java
+++ src/numbersExercises/HappyNumber.java
@@ -1,8 +1,32 @@
 package numbersExercises;
 
+import java.util.HashSet;
+import java.util.Set;
+
+//        Example (19)
+//        1^2 + 9^2=82
+//        8^2 + 2^2=68
+//        6^2 + 8^2=100
+//        1^2 + 0^2 + 0^2 = 1
+
 public class HappyNumber {
     public boolean checkHappy (int number) {
-
+        Set<Integer> store = new HashSet<>();
+        store.add(number);
+        while(!store.contains(1)){
+            number = numSquareSum(number);
+            if(store.contains(number)) return false;    //when loop existing
+            store.add(number);
+        }
         return true;
+    }
+
+    public int numSquareSum ( int number) {
+        int squareSum = 0;
+        while(number != 0) {
+            squareSum += (number%10)*(number%10);
+            number /= 10;
+        }
+        return squareSum;
     }
 }
