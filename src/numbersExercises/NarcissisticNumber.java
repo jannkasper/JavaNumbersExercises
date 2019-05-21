0cd0a4e7888dfe00c03ee3c06dd780401022bfb1
Jan K <jann.kasper@gmail.com>
2019-05-21 07:10:32 +0200

    NarcissisticNumber finish

--- src/numbersExercises/NarcissisticNumber.java
+++ src/numbersExercises/NarcissisticNumber.java
@@ -1,7 +1,18 @@
 package numbersExercises;
 
+//      Narcissistic number is a number that is the sum of its own digits each raised to the power of the number of digits.
+//      Example (153)
+//      153 == 1^3 + 5^3 + 3^3
+
 public class NarcissisticNumber {
     public boolean checkNarcissistic (int number) {
-        return true;
+        int length = Integer.toString(number).length();
+        int sum = 0, copyNumber = number;
+        for (int i = 0; i<length; i++){
+            sum += Math.pow(copyNumber%10, length);
+            copyNumber /= 10;
+        }
+
+        return (number == sum) ? true : false;
     }
 }
